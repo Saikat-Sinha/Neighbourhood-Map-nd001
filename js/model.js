5f81844bef7910f7b5ffb0306f2a7b73cf685da6
AskSaikatSinha <saikat@myself.com>
2017-01-28 23:39:39 +0530

    Added Navbar

---
+++ js/model.js
@@ -0,0 +1,86 @@
+var places = [
+    {
+        "name": "Golconda Fort",
+        "location" : {
+            "lat": 17.385363,
+            "lng":78.404130
+        }
+    },
+    {
+        "name": "Char Minar",
+        "location" : {
+            "lat": 17.361564,
+            "lng": 78.474665
+        }
+    },
+    {
+        "name": "Qutub Shahi Tombs",
+        "location" : {
+            "lat": 17.394746,
+            "lng": 78.394717
+        }
+    },
+    {
+        "name": "Nehru Zoological Park",
+        "location" : {
+            "lat": 17.348305,
+            "lng": 78.442511
+        }
+    },
+    {
+        "name": "Salar Jung Museum",
+        "location" : {
+            "lat": 17.3713733,
+            "lng": 78.4804193
+        }
+    },
+    {
+        "name": "NTR Gardens",
+        "location" : {
+            "lat": 17.4126164,
+            "lng": 78.4687845
+        }
+    },
+    {
+        "name": "Falaknuma Palace",
+        "location" : {
+            "lat": 17.330955,
+            "lng": 78.467504
+        }
+    },
+    {
+        "name": "Lumbini Park",
+        "location" : {
+            "lat": 17.4100675,
+            "lng": 78.4732084
+        }
+    },
+    {
+        "name": "Prasads IMAX",
+        "location" : {
+            "lat": 17.413143,
+            "lng": 78.4657801
+        }
+    },
+    {
+        "name": "Mecca Masjid",
+        "location" :{
+            "lat": 17.3604416,
+            "lng": 78.4735712
+        }
+    },
+    {
+        "name": "KBR National Park",
+        "location" :{
+            "lat": 17.4205891,
+            "lng": 78.4215581
+        }
+    },
+    {
+        "name": "Birla Mandir",
+        "location" :{
+            "lat": 17.4062367,
+            "lng": 78.4690601
+        }
+    }
+]
